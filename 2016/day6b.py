41a457b1d26821bbb992973ef12214eaba9d843e
Jan Nijboer <jrnijboer@gmail.com>
2016-12-07 00:05:43 +0100

    day 6 added

---
+++ 2016/day6b.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+with open("day6.input") as inputfile:
+	columns = zip(*[list(message[:-1]) for message in inputfile])
+
+answer = ''
+for column in columns:
+	answer += Counter(column).most_common()[-1][0]
+print answer
