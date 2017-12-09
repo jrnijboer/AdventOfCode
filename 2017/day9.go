60049b470bbab3203ddc9b7bbdcf705af348c332
Jan Nijboer <jan@tatooine.e0xpa4lgqftuzdgveunw0p5qbb.ax.internal.cloudapp.net>
2017-12-09 10:08:45 +0100

    day 9 solution

---
+++ 2017/day9.go
@@ -0,0 +1,44 @@
+package main
+import (
+        "fmt"
+        "bufio"
+        "os"
+)
+
+func main() {
+	file, _ := os.Open("day9.input")
+        defer file.Close()
+        scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		solve(input)
+	}
+}
+
+func solve(input string){
+	length := len(input)
+	openbraces := 0
+	pos := 0
+	garbage := false
+	sum := 0
+	garbageSum := 0
+	for pos < length {
+		if garbage && string(input[pos]) != "!" { garbageSum++ }
+
+		switch(string(input[pos])) {
+			case "!" : pos++
+			case "<" : garbage = true
+			case ">" :
+				garbage = false
+				garbageSum--
+			case "{" : if !garbage { openbraces++ }
+			case "}" : if !garbage {
+					sum += openbraces
+					openbraces--
+				}
+		}
+		pos++
+	}
+
+	fmt.Printf("score: %v, garbage: %v\n", sum, garbageSum)
+}
