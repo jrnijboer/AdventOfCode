c61a8f5b2ff992f5c3aec0b93894f1fb02f9dea9
root <root@R2D2.localdomain>
2018-12-01 22:36:11 +0100

    day1, csharp

---
+++ 2018/csharp/day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc2018
+{
+    class Day1
+    {
+        public static void Solve()
+        {
+            var values = File.ReadAllLines(Path.Combine("..", "input", "day1.input")).Select(line => int.Parse(line));
+            Console.WriteLine("Day 1, answer a: {0}", values.Sum());
+
+            var f = 0;
+            var freqs = new Dictionary<int, int>();
+
+            while (true)
+            {
+                foreach (var value in values)
+                {
+                    f += value;
+
+                    if (!freqs.ContainsKey(f))
+                    {
+                        freqs[f] = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Day 1, answer b: {0}", f);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2018/csharp/program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aoc2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Solve();
+            Console.ReadKey();           
+        }
+    }
+}
