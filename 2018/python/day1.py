0e8bb6dbfc5658c587985161d2d095d8ba233c8c
Jan Nijboer <jrnijboer@gmail.com>
2018-12-01 22:41:42 +0100

    changed dict to set

--- 2018/python/day1.py
+++ 2018/python/day1.py
@@ -1,12 +1,12 @@
 values = list(map(int, open("../input/day1.input").readlines()))
 print "answer a:", sum(values)
 f = 0;
-freqs = {};
+freqs = set();
 i = 0;
 while True:
     f += values[i % len(values)]
     if f in freqs:
         break
-    freqs[f] = 0
+    freqs.add(f)
     i += 1
 print "answer b:", f
