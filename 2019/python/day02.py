0d11b0e15df2808cd29f809bcaa6c7ab4fc91e36
Jan Nijboer <jnijboer@schoolmaster.nl>
2019-12-02 11:59:26 +0100

    cleanup day 2

--- 2019/python/day02.py
+++ 2019/python/day02.py
@@ -5,7 +5,7 @@ for noun in range(100):
         while opcode != 99:
             if opcode == 1: input[input[ip + 3]] = input[input[ip + 1]] + input[input[ip + 2]]
             if opcode == 2: input[input[ip + 3]] = input[input[ip + 1]] * input[input[ip + 2]]
-            ip = ip + 4
+            ip += 4
             opcode = input[ip]
-        if (noun == 12 and verb == 2): print ("answer a: {}".format(input[0]))
+        if noun == 12 and verb == 2: print ("answer a: {}".format(input[0]))
         if input[0] == 19690720: print ("answer b: {}".format(100 * noun + verb))
