df2dc68112a9c9ae1eee4888ff06192bef513354
Jan Nijboer <jnijboer@schoolmaster.nl>
2019-12-03 14:15:25 +0100

    cleanup

--- 2019/python/day03.py
+++ 2019/python/day03.py
@@ -1,12 +1,10 @@
 def getRoute (directions):
+    delta = dict(zip('LRUD', [(-1, 0),(1, 0),(0, 1),(0, -1)]))
     route, p = [], [0, 0]
     for d in directions.split(","):
         D, steps = d[0], int(d[1:])
         while steps > 0:
-            if D == "U": p[1] += 1
-            if D == "R": p[0] += 1
-            if D == "L": p[0] -= 1
-            if D == "D": p[1] -= 1
+            p = (p[0] + delta[D][0], p[1] + delta[D][1])
             steps -= 1
             route.append((p[0], p[1]))
     return route
@@ -14,6 +12,5 @@ def getRoute (directions):
 input = open("../input/day3.input").readlines()
 r1, r2 = getRoute(input[0]), getRoute(input[1])
 intersect = set(r1).intersection(set(r2))
-a = min(intersect, key=lambda x:abs(x[0]) + abs(x[1]))
-print("answer a: {}".format(abs(a[0]) + abs(a[1])))
+print("answer a: {}".format(min([abs(i[0]) + abs(i[1]) for i in intersect])))
 print("answer b: {}".format(2 + min([r1.index(i) + r2.index(i) for i in intersect])))
