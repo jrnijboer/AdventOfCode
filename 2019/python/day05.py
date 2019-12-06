abca230b88ce9f4fecdbb239ef89f09b366debde
Jan Nijboer <jnijboer@schoolmaster.nl>
2019-12-06 09:19:14 +0100

    less is more!

--- 2019/python/day05.py
+++ 2019/python/day05.py
@@ -1,23 +1,18 @@
 def solve(inputValue):
-    instructions = list(map(int,open("../input/day5.input").readline().split(",")))
-    opcode, ip, val1, val2 = instructions[0], 0, 0, 0
-    while opcode != 99:
-        m1, m2= instructions[ip] % 1000 // 100, instructions[ip] % 10000 // 1000
-        if opcode != 3 and opcode != 4:
-            val1 = instructions[ip + 1] if m1 == 1 else instructions[instructions[ip + 1]]
-            val2 = instructions[ip + 2] if m2 == 1 else instructions[instructions[ip + 2]]
-        if opcode == 1: instructions[instructions[ip + 3]] = val1 + val2
-        elif opcode == 2: instructions[instructions[ip + 3]] = val1 * val2
-        elif opcode == 3: instructions[instructions[ip + 1]] = inputValue
-        elif opcode == 4:
-            s = instructions[ip + 1] if m1 == 1 else instructions[instructions[ip + 1]]
-            if s != 0: return (s)
-        elif opcode == 5: ip = val2 if val1 != 0 else ip + 3
-        elif opcode == 6: ip = val2 if val1 == 0 else ip + 3
-        elif opcode == 7: instructions[instructions[ip + 3]]  = 1 if val1 < val2 else 0
-        elif opcode == 8: instructions[instructions[ip + 3]] = 1 if val1 == val2 else 0
-        if opcode in [1,2,7,8]: ip += 4
-        if opcode in [3,4]: ip += 2
-        opcode = instructions[ip] % 100
-print("answer a: {}".format(solve(1)))
-print("answer b: {}".format(solve(5)))
+    oc, ip, I = 0, 0, list(map(int,open("../input/day5.input").readline().split(",")))    
+    while oc != 99:
+        m1, m2 = I[ip] % 1000 // 100, I[ip] % 10000 // 1000
+        val1 = I[ip + 1] if m1 == 1 else I[I[ip + 1]]
+        if oc != 3 and oc != 4: val2 = I[ip + 2] if m2 == 1 else I[I[ip + 2]]
+        if oc == 1: I[I[ip + 3]] = val1 + val2
+        elif oc == 2: I[I[ip + 3]] = val1 * val2
+        elif oc == 3: I[I[ip + 1]] = inputValue
+        elif oc == 4 and val1 != 0: print(val1)
+        elif oc == 5: ip = val2 - 2 if val1 != 0 else ip + 1
+        elif oc == 6: ip = val2 - 2 if val1 == 0 else ip + 1
+        elif oc == 7: I[I[ip + 3]]  = 1 if val1 < val2 else 0
+        elif oc == 8: I[I[ip + 3]] = 1 if val1 == val2 else 0
+        ip = ip + 4 if oc in [1,2,7,8] else ip + 2
+        oc = I[ip] % 100
+print("answer a: ", end = ''), solve(1)
+print("answer b: ", end = ''), solve(5)
