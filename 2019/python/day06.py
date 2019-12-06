03fae60b61a24708d4f83ce87468f2c7210d91ec
Jan Nijboer <jrnijboer@gmail.com>
2019-12-06 23:26:26 +0100

    used generator instead of list

--- 2019/python/day06.py
+++ 2019/python/day06.py
@@ -1,9 +1,7 @@
 def route(input, o):
-    steps = []
     while o in input:
-        steps.append(input[o])
+        yield input[o]
         o = input[o]
-    return set(steps)
 input = {k:v for (v,k) in [line.strip("\n").split(")") for line in open("../input/day6.input").readlines()]}
-print("answer a: {}".format(sum([len(route(input, i)) for i in input])))
-print("answer b: {}".format((len(route(input, 'SAN') ^ route(input, 'YOU')))))
+print("answer a: {}".format(sum([len(list(route(input, i))) for i in input])))
+print("answer b: {}".format(len(set(route(input, 'SAN')) ^ set(route(input, 'YOU')))))
