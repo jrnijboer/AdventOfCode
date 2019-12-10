ab33df0f509592b7e72068a5a0987b95625dae15
Jan Nijboer <jnijboer@schoolmaster.nl>
2019-12-10 09:26:12 +0100

    day 9 refactor

--- 2019/python/day09.py
+++ 2019/python/day09.py
@@ -4,24 +4,16 @@ def solve(invalue, I, ip):
         m1, m2, m3 = I[ip] % 1000 // 100, I[ip] % 10000 // 1000, I[ip] // 10000
         val1 = I[I[ip + 1]] if m1 == 0 else I[ip + 1] if m1 == 1 else I[I[ip + 1] + offset]
         if oc not in [3,4,9]: val2 = I[I[ip + 2]] if m2 == 0 else I[ip + 2] if m2 == 1 else I[I[ip + 2] + offset]
-        if oc == 1:
-            if m3 == 0: I[I[ip + 3]] = val1 + val2
-            else: I[I[ip + 3] + offset] = val1 + val2
-        elif oc == 2:
-            if m3 == 0: I[I[ip + 3]] = val1 * val2
-            else: I[I[ip + 3] + offset] = val1 * val2
-        elif oc == 3:
-            if m1 == 0: I[I[ip + 1]] = invalue
-            else: I[I[ip + 1] + offset] = invalue
+        if oc in [1,2,7,8]: writeaddr = I[ip + 3] if m3 == 0 else I[ip + 3] + offset  
+        if oc == 3: writeaddr =I[ip + 1] if m1 == 0 else I[ip + 1] + offset
+        if oc == 1: I[writeaddr] = val1 + val2
+        elif oc == 2: I[writeaddr] = val1 * val2
+        elif oc == 3: I[writeaddr]= invalue
         elif oc == 4 : return val1
         elif oc == 5: ip = val2 - 2 if val1 != 0 else ip + 1
         elif oc == 6: ip = val2 - 2 if val1 == 0 else ip + 1
-        elif oc == 7:
-            if m3 == 0: I[I[ip + 3]] = 1 if val1 < val2 else 0
-            else: I[I[ip + 3] + offset] = I[I[ip + 3]] = 1 if val1 < val2 else 0
-        elif oc == 8:
-            if m3 == 0: I[I[ip + 3]] = 1 if val1 == val2 else 0
-            else: I[I[ip + 3] + offset] = 1 if val1 == val2 else 0
+        elif oc == 7: I[writeaddr] = 1 if val1 < val2 else 0
+        elif oc == 8: I[writeaddr] = 1 if val1 == val2 else 0
         elif oc == 9: offset += val1
         ip = ip + 4 if oc in [1,2,7,8] else ip + 2
         oc = I[ip] % 100
