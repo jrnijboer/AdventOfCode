b579d3145a317dd8c6585ed201613be1baa9b677
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-01 18:55:54 +0100

    small cleanup

--- 2020/python/day01.py
+++ 2020/python/day01.py
@@ -1,4 +1,4 @@
 import itertools
 input = [int(i.strip()) for i in open("../input/day01.input").readlines()]
-print("Answer A:", [i[0] * i[1] for i in itertools.combinations(input, 2) if i[0] + i[1] == 2020][0])
-print("Answer B:", [i[0] * i[1] * i[2] for i in itertools.combinations(input, 3) if i[0] + i[1] + i[2] == 2020][0])
+print("Answer A:", [i * j for i, j in itertools.combinations(input, 2) if i + j == 2020][0])
+print("Answer B:", [i * j * k for i, j, k in itertools.combinations(input, 3) if i + j + k == 2020][0])
