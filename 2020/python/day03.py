b0d66b59e5e73c45cb3143dd2541d52b747adc75
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-03 12:57:14 +0100

    refactored

--- 2020/python/day03.py
+++ 2020/python/day03.py
@@ -1,11 +1,6 @@
+def trees(dx, dy, input):
+  return len([(x,y) for x, y in [((y * dx // dy) % len(input[y]), y) for y in range(0, len(input), dy) ] if input[y][x] == "#"])
+
 input = [line.strip() for line in open("../input/day03.input").readlines()]
-vectors = [(1,1), (3,1), (5,1), (7,1), (1,2)]
-b = 1
-for vector in vectors:
-  trees, x = 0, 0
-  for y in range(0, len(input), vector[1]):
-    if input[y][x] == "#": trees += 1
-    x = (x + vector[0]) % len(input[y])
-  if vector == (3,1): print("Answer A:", trees)
-  b *= trees
-print("Answer B:", b)
+print("Answer A:", trees(3, 1, input))
+print("Answer B:", trees(1, 1, input) * trees(3, 1, input) * trees(5, 1, input) * trees(7, 1, input) * trees(1, 2, input))
