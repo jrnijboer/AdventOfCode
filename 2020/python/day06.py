e28a7977bc177dc26202235502b1eb4659ed3729
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-06 17:21:59 +0100

    learned about the asterisk, the unpacking operator

--- 2020/python/day06.py
+++ 2020/python/day06.py
@@ -1,7 +1,3 @@
 input, b = [line for line in open("../input/day06.input").read().split("\n\n") if line ], 0
 print("Answer A:", len([c for c in "abcdefghijklmnopqrstuvwxyz" for line in input if c in line]))
-for line in input:
-  answers = set("abcdefghijklmnopqrstuvwxyz")
-  for group in line.split(): answers = answers.intersection(group)
-  b += len(answers)
-print("Answer B:", b)
+print("Answer B:", sum([len(set.intersection(*gset)) for gset in [[set(group) for group in line.split()] for line in input]]))
