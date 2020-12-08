7e359dd1f87aa43710b6f5749387605467e8558d
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-08 20:04:55 +0100

    cleanup

--- 2020/python/day08.py
+++ 2020/python/day08.py
@@ -1,6 +1,6 @@
 input = [line.strip() for line in open("../input/day08.input").readlines()]
 
-def Solve(input):
+def solve(input):
   acc, pointer, visited = 0, 0, set()
   while pointer not in visited and pointer < len(input):
     visited.add(pointer)
@@ -12,9 +12,8 @@ def Solve(input):
 def patch(input, i):
   patched = input.copy()
   operator, argument = patched[i].split(" ")
-  if operator == "jmp": patched[i] = "nop " + argument
-  elif operator == "nop": patched[i] = "jmp " + argument
+  patched[i] = "nop " + argument if operator == "jmp" else "jmp " + argument
   return patched
 
-print("Answer A:", Solve(input)[0])
-print("Answer B:", [b for b, ip in [Solve(x) for x in [p for p in [patch(input, i) for i in range(len(input)) if not input[i].startswith("acc")]]] if ip >= len(input)][0])
+print("Answer A:", solve(input)[0])
+print("Answer B:", [b for b, ip in [solve(patch(input, i)) for i in range(len(input)) if not input[i].startswith("acc")] if ip >= len(input)][0])
