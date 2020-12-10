f8e2ddbe6c9926ed1f9f2a855de7c0a8d20a5d1c
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-10 21:56:41 +0100

    fixed dictionary initialization

--- 2020/python/day10.py
+++ 2020/python/day10.py
@@ -2,13 +2,13 @@ from collections import defaultdict
 numbers = sorted([int(line.strip()) for line in open("../input/day10.input").readlines()])
 
 j, d1, d3 = 0, 0, 1
-for n in range(len(numbers)):  
+for n in range(len(numbers)):
   if numbers[n] - j == 1: d1 += 1
   elif numbers[n] - j == 3: d3 += 1
   j += numbers[n] - j
 print("Answer A:", d1 * d3)
 
-D = defaultdict(int, {1:1, 2:2, 3:4}) # works because lowest 3 values in input are 1,2,3
-for n in numbers[3:]:  
+D = defaultdict(int, {0:1})
+for n in numbers:
   D[n] = D[n-1] + D[n-2] + D[n-3]
 print("Answer B:", D[max(D.keys())])
