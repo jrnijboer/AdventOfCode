82db6c41f74f347234f31c4e496d78b9c04e50b3
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-22 15:46:32 +0100

    more minor improvements

--- 2020/python/day22.py
+++ 2020/python/day22.py
@@ -1,27 +1,23 @@
-def appointCardsToWinner(A, B, a, b, winner):
-  if winner == 0:
-    A.append(a)
-    A.append(b)
-  else:
-    B.append(b)
-    B.append(a)
-  return A, B
+def appointCardsToWinner(D, a, b, winner):
+  D[winner].append(a if winner == 0 else b)
+  D[winner].append(b if winner == 0 else a)
+  return D
 
-def play(A, B, recurse):
-  statesA, statesB = set(), set()
+def play(D, recurse):
+  A, B, statesA, statesB = D[0], D[1], set(), set()
   while A and B:
     if tuple(A) in statesA and tuple(B) in statesB: return [1], []
     statesA.add(tuple(A))
     statesB.add(tuple(B))
     a, b = A.pop(0), B.pop(0)
     if recurse and a <= len(A) and b <= len(B):
-      Asub, Bsub = play(list(A)[:a], list(B)[:b], recurse)
-      A, B = appointCardsToWinner(A, B, a, b, 0 if len(Asub) > len(Bsub) else 1)
-    else: A, B = appointCardsToWinner(A, B, a, b, 0 if a > b else 1)
-  return A, B
+      Asub, Bsub = play([list(A)[:a], list(B)[:b]], recurse)
+      D = appointCardsToWinner(D, a, b, 0 if len(Asub) > len(Bsub) else 1)
+    else: D = appointCardsToWinner(D, a, b, 0 if a > b else 1)
+  return [A, B]
 
 A, B = [list(map(int, x.split("\n")[1:])) for x in [line.strip() for line in open("../input/day22.input").read().split("\n\n")]]
-A1, B1 = play(A.copy(),B.copy(), False)
-print("Answer A:", sum([(len(A1) - i) * val for i, val in enumerate(A1)]) + sum([(len(B1) - i) * val for i, val in enumerate(B1)]))
-A2, B2 = play(A,B, True)
-print("Answer B:", sum([(len(A2) - i) * val for i, val in enumerate(A2)]) + sum([(len(B2) - i) * val for i, val in enumerate(B2)]))
+D = play([A.copy(),B.copy()], False)
+print("Answer A:", sum([(len(D[0]) - i) * val for i, val in enumerate(D[0])]) + sum([(len(D[1]) - i) * val for i, val in enumerate(D[1])]))
+D = play([A,B], True)
+print("Answer B:", sum([(len(D[0]) - i) * val for i, val in enumerate(D[0])]) + sum([(len(D[1]) - i) * val for i, val in enumerate(D[1])]))
