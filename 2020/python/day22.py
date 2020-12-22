59993a4fa5f4523643a477fa2c658d9a6d40a9e3
Jan Nijboer <jnijboer@schoolmaster.nl>
2020-12-22 15:22:15 +0100

    minor improvements

--- 2020/python/day22.py
+++ 2020/python/day22.py
@@ -1,4 +1,3 @@
-from collections import deque
 def appointCardsToWinner(A, B, a, b, winner):
   if winner == 0:
     A.append(a)
@@ -11,19 +10,18 @@ def appointCardsToWinner(A, B, a, b, winner):
 def play(A, B, recurse):
   statesA, statesB = set(), set()
   while A and B:
-    if tuple(A) in statesA and tuple(B) in statesB: return A + B, deque()
+    if tuple(A) in statesA and tuple(B) in statesB: return [1], []
     statesA.add(tuple(A))
     statesB.add(tuple(B))
-    a, b = A.popleft(), B.popleft()
+    a, b = A.pop(0), B.pop(0)
     if recurse and a <= len(A) and b <= len(B):
-      Asub, Bsub = play(deque(list(A)[:a]), deque(list(B)[:b]), recurse)
+      Asub, Bsub = play(list(A)[:a], list(B)[:b], recurse)
       A, B = appointCardsToWinner(A, B, a, b, 0 if len(Asub) > len(Bsub) else 1)
     else: A, B = appointCardsToWinner(A, B, a, b, 0 if a > b else 1)
   return A, B
 
-A1, B1 = [deque(map(int, x.split("\n")[1:])) for x in [line.strip() for line in open("../input/day22.input").read().split("\n\n")]]
-A2, B2 = [deque(map(int, x.split("\n")[1:])) for x in [line.strip() for line in open("../input/day22.input").read().split("\n\n")]]
-A1, B1 = play(A1,B1, False)
+A, B = [list(map(int, x.split("\n")[1:])) for x in [line.strip() for line in open("../input/day22.input").read().split("\n\n")]]
+A1, B1 = play(A.copy(),B.copy(), False)
 print("Answer A:", sum([(len(A1) - i) * val for i, val in enumerate(A1)]) + sum([(len(B1) - i) * val for i, val in enumerate(B1)]))
-A2, B2 = play(A2,B2, True)
+A2, B2 = play(A,B, True)
 print("Answer B:", sum([(len(A2) - i) * val for i, val in enumerate(A2)]) + sum([(len(B2) - i) * val for i, val in enumerate(B2)]))
