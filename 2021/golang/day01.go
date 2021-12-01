b19f1aaa2390f6a85b9f883b6ea11d064d36ac70
Jan Nijboer <jrnijboer@gmail.com>
2021-12-01 08:35:23 +0100

    day 1 Sonar Sweep - golang

---
+++ 2021/golang/day01.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Day01() {
+	file, _ := os.Open("../input/day01.input")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var digits []int
+
+	for scanner.Scan() {
+		digit, _ := strconv.Atoi(scanner.Text())
+		digits = append(digits, digit)
+	}
+
+	partA := 0
+	for i := 1; i < len(digits); i++ {
+		if digits[i] > digits[i-1] {
+			partA++
+		}
+	}
+
+	partB := 0
+	for i := 3; i < len(digits); i++ {
+		if digits[i]+digits[i-1]+digits[i-2] > digits[i-1]+digits[i-2]+digits[i-3] {
+			partB++
+		}
+	}
+
+	fmt.Println("Answer A:", partA)
+	fmt.Println("Answer B:", partB)
+}
---
+++ 2021/golang/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Day 1 answers:")
+	Day01()
+}
