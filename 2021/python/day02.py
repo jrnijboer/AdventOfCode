e28785823e150899a39f2ebfc5434804e7362af7
Jan Nijboer <jrnijboer@gmail.com>
2021-12-02 09:53:27 +0100

    Day 22 Dive! - python

---
+++ 2021/python/day02.py
@@ -0,0 +1,14 @@
+input = [i.strip() for i in open("../input/day02.input").readlines()]
+D = { "forward": 1j, "down": 1, "up": -1 }
+
+pos = sum([D[line.split()[0]] * int(line.split()[1]) for line in input])
+print ("Answer A:", int(pos.imag * pos.real))
+
+x, y, aim = 0, 0, 0
+for direction, steps in [line.split() for line in input]:
+    if direction == "forward":
+        x += int(steps)
+        y += aim * int(steps)
+    else:
+        aim += int(steps) * D[direction]
+print ("Answer B:", x * y)
