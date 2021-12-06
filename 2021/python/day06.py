af79468df9f6f65a459dc7914cd5206c78dc2cb7
Jan Nijboer <jrnijboer@gmail.com>
2021-12-06 10:36:07 +0100

    Use Counter instead of defaultdict

--- 2021/python/day06.py
+++ 2021/python/day06.py
@@ -1,8 +1,5 @@
-from collections import defaultdict
-F = defaultdict(int)
-for n in open("../input/day06.input", encoding="utf-8").read().strip().split(","):
-    F[int(n)] += 1
-
+from collections import Counter
+F = Counter([int(n) for n in open("../input/day06.input", encoding="utf-8").read().split(",")])
 for day in range(256):
     new, FNext = F[0], {i: F[(i + 1) % 9] for i in range(9)}
     FNext[6] += new
