60e3004788a649a2ec49b69316de856714fa97fc
Jan Nijboer <jrnijboer@gmail.com>
2021-12-06 23:04:54 +0100

    fix linting issues

--- 2021/python/day01.py
+++ 2021/python/day01.py
@@ -1,4 +1,4 @@
 numbers = [int(i.strip()) for i in open("../input/day01.input", encoding="utf-8").readlines()]
 avg = [numbers[i] + numbers[i-1] + numbers[i-2] for i in range(2, len(numbers))]
-print ("Answer A:", sum([a < b for a, b in zip(numbers, numbers[1:])]))
-print ("Answer B:", sum([a < b for a, b in zip(avg, avg[1:])]))
+print("Answer A:", sum([a < b for a, b in zip(numbers, numbers[1:])]))
+print("Answer B:", sum([a < b for a, b in zip(avg, avg[1:])]))
--- 2021/python/day02.py
+++ 2021/python/day02.py
@@ -1,8 +1,8 @@
 lines = [i.strip() for i in open("../input/day02.input", encoding="utf-8").readlines()]
-D = { "forward": 1j, "down": 1, "up": -1 }
+D = {"forward": 1j, "down": 1, "up": -1}
 
 pos = sum([D[line.split()[0]] * int(line.split()[1]) for line in lines])
-print ("Answer A:", int(pos.imag * pos.real))
+print("Answer A:", int(pos.imag * pos.real))
 
 x, y, aim = 0, 0, 0
 for direction, steps in [line.split() for line in lines]:
@@ -11,4 +11,4 @@ for direction, steps in [line.split() for line in lines]:
         y += aim * int(steps)
     else:
         aim += int(steps) * D[direction]
-print ("Answer B:", x * y)
+print("Answer B:", x * y)
