c06239f00e28c78397721e014f499ebcbd6564b7
Jan Nijboer <jrnijboer@gmail.com>
2022-12-09 20:41:08 +0100

    Day 9: Rope Bridge

---
+++ 2022/python/day09.py
@@ -0,0 +1,29 @@
+steps = [line.strip() for line in open("../input/day09.input", encoding="utf-8").readlines()]
+A, B, R = {(0,0)}, {(0,0)}, [(0,0)]*10
+D = {"U": [0, -1], "D": [0, 1], "L": [-1, 0], "R": [1, 0]}
+
+for parts in [step.split() for step in steps]:
+    for _ in range(int(parts[1])):
+        R[0] = (R[0][0] + D[parts[0]][0], R[0][1]  + D[parts[0]][1])
+        for i in range(1, len(R)):
+            if R[i - 1][0] == R[i][0] and R[i][1] - R[i - 1][1] > 1: #R[i] goes up
+                R[i] = (R[i][0], R[i][1] - 1)
+            elif R[i - 1][0] == R[i][0] and R[i][1] - R[i - 1][1] < -1: #R[i] goes down
+                R[i] = (R[i][0], R[i][1] + 1)
+            elif R[i - 1][0] - R[i][0] < -1 and R[i][1] == R[i - 1][1]: #R[i] goes left
+                R[i] = (R[i][0] - 1, R[i][1])
+            elif R[i - 1][0] - R[i][0] > 1 and R[i][1] == R[i - 1][1]: #R[i] goes right
+                R[i] = (R[i][0] + 1, R[i][1])
+            elif R[i - 1][0] - R[i][0] <= -1 and R[i - 1][1] - R[i][1] <= -2 or R[i - 1][0] - R[i][0] <= -2 and R[i - 1][1] - R[i][1] <= -1 : #R[i] goes up-left
+                R[i] = (R[i][0] - 1, R[i][1] - 1)
+            elif R[i - 1][0] - R[i][0] >= 1 and R[i - 1][1] - R[i][1] <= -2 or R[i - 1][0] - R[i][0] >= 2 and R[i - 1][1] - R[i][1] <= -1: #R[i] goes upright
+                R[i] = (R[i][0] + 1, R[i][1] - 1)
+            elif R[i - 1][0] - R[i][0] <= -1 and R[i - 1][1] - R[i][1] >= 2 or R[i - 1][0] - R[i][0] <= -2 and R[i - 1][1] - R[i][1] >= 1: #R[i] goes down-left
+                R[i] = (R[i][0] - 1, R[i][1] + 1)
+            elif R[i - 1][0] - R[i][0] >= 1 and R[i - 1][1] - R[i][1] >= 2 or R[i - 1][0] - R[i][0] >= 2 and R[i - 1][1] - R[i][1] >= 1: #R[i] goes down-right
+                R[i] = (R[i][0] + 1, R[i][1] + 1)
+            A.add(R[1])
+            B.add(R[9])
+
+print("Answer A:", len(A))
+print("Answer B:", len(B))
