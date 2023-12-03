e7ab253ced49cfdd145c21f7fba0afe34d09d1ae
Jan Nijboer <jan.nijboer@iddinkgroup.com>
2023-12-03 20:51:05 +0100

    fix gears

--- 2023/python/day03.py
+++ 2023/python/day03.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 lines = ["." + line.strip() + "." for line in open("../input/day03.txt", encoding="utf-8").readlines()]
 lines = ["." * (len(lines[0]) + 2)] + lines + ["." * (len(lines[0]) + 2)]
-sumA, sumB, allGears, gears = 0, 0, defaultdict(list), set()
+sumA, sumB, allGears = 0, 0, defaultdict(set)
 
 for lnr, line in enumerate(lines):
     pos, number = 0, ""
@@ -17,14 +17,12 @@ for lnr, line in enumerate(lines):
                             if not lines[lnr + dy][p + dx].isdigit() and lines[lnr + dy][p + dx] != ".":
                                 isPart = True
                             if lines[lnr + dy][p + dx] == "*":
-                                gears.add((lnr + dy, p + dx))
+                                allGears[(lnr + dy, p + dx)].add(int(number))
                 if isPart:
                     sumA += int(number)
-                    for g in gears:
-                        allGears[g].append(int(number))
-            gears, number = set(), ""
+            number = ""
         pos += 1
 
 print(f"Answer A: {sumA}")
-sumB = sum([x[0] * x[1] for x in allGears.values() if (len(x) == 2)])
+sumB = sum([n[0] * n[1] for n in [list(x) for x in allGears.values() if (len(x) == 2)]])
 print(f"Answer B: {sumB}")
