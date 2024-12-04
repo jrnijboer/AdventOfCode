70da055c342920958462cf6dd91358077492ed78
Jan Nijboer <jrnijboer@gmail.com>
2024-12-04 21:39:09 +0100

    Fixes for day 3

--- 2024/python/day03.py
+++ 2024/python/day03.py
@@ -1,9 +1,9 @@
 import re
 memory = open("../input/03.txt").read()
-pattern = r"mul\(\d+,\d+\)"
-print("Answer A:", sum(l * r for l, r in (map(int, re.findall(r"\d{1,3}", match)) for match in re.findall(pattern, memory))))
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+print("Answer A:", sum(int(match[0]) * int(match[1]) for match in re.findall(pattern, memory)))
 
 B = 0
 for enabledmemory in re.findall(r"do\(\).*?don't\(\)", "do()" +  memory + "don't()", re.DOTALL):
-    B += sum(l * r for l, r in (map(int, re.findall(r"\d{1,3}", match)) for match in re.findall(pattern, enabledmemory)))
+    B += sum(int(match[0]) * int(match[1]) for match in re.findall(pattern, enabledmemory))
 print("Answer B:", B)
--- 2024/python/day03noregex.py
+++ 2024/python/day03noregex.py
@@ -4,12 +4,12 @@ A, B, mul = 0, 0, 1
 for i in range(len(memory)):
     if memory[i : i + 4] == "mul(":
         parts = memory[i + 4 : memory.find(")", i + 4)].split(",")
-        if len(parts) == 2 and all(part.isdigit() for part in parts):
+        if len(parts) == 2 and all(part.isdigit() and 1 <= len(part) <= 3 for part in parts):
             A += int(parts[0]) * int(parts[1])
             B += int(parts[0]) * int(parts[1]) * mul
-    elif memory[i:].startswith("do()"):
+    elif memory[i : i + 4] == "do()":
         mul = 1
-    elif memory[i:].startswith("don't()"):
+    elif memory[i : i + 7] == "don't()":
         mul = 0
 
 print("Answer A:", A)
